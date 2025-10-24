e0bd3b0804cc8eb1c3c6b6d4062bdb2a55be383c
GOLIBJON-developer <blackstaroffical88@gmail.com>
2025-10-25 01:57:05 +0500

    first commit

---
+++ examples/1_accounts.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const { ethers } = require("ethers")
+
+// Setup connection
+const URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+const provider = new ethers.JsonRpcProvider(URL)
+
+const ADDRESS = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e"
+
+async function main() {
+  // Get balance
+  const balance = await provider.getBalance(ADDRESS)
+
+  // Log balance
+  console.log(`\nETH Balance of ${ADDRESS} --> ${ethers.formatUnits(balance, 18)} ETH\n`)
+}
+
+main()
